2a9f689dc9f098f920f42dee793c6a85e67e95fe
Mhayk Whandson <hi@mhayk.com>
2019-11-21 09:04:37 +0000

    The support to import componentes by '~' has been configurated.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const { override, addBabelPlugin } = require('customize-cra')
+
+module.exports = override(
+  addBabelPlugin (
+       ['babel-plugin-root-import',{
+      'rootPathSuffix': "src",
+      'rootPathPrefix': "~",
+       }
+      ]
+   )
+)
