0bbc85205871e307fb937e73caf5caa5b92f5ae0
Mhayk Whandson <hi@mhayk.com>
2019-11-21 14:59:55 +0000

    Global styled component has been configurated.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   </Provider>
 );
 
