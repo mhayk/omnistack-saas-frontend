32091750487ff13ecfc8f74a3668d198a4c161f1
Mhayk Whandson <hi@mhayk.com>
2019-11-23 23:52:29 +0000

    redirect to route / with saga + connected-react-router.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,4 +1,5 @@
 import { call, put } from 'redux-saga/effects';
+import { push } from 'connected-react-router';
 import { actions as toastrActions } from 'react-redux-toastr';
 import api from '~/services/api';
 
@@ -11,6 +12,8 @@ export function* signIn({ email, password }) {
     localStorage.setItem('@Omni:token', response.data.token);
 
     yield put(AuthActions.signInSuccess(response.data.token));
+
+    yield put(push('/'));
   } catch (error) {
     yield put(toastrActions.add({
       type: 'error',
