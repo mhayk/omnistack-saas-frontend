b42916a848eb33db6aabe898a6cee3d5c33628b6
Mhayk Whandson <hi@mhayk.com>
2019-11-26 09:23:36 +0000

    TeamSwitch components have been created and styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import TeamSwitcher from '~/components/TeamSwitcher';
+import { Container } from './styles';
 
-const Main = () => <div />;
+const Main = () => (
+  <Container>
+    <TeamSwitcher />
+  </Container>
+);
 
 export default Main;
