31f2a6a2a4d06c6bc158c6401a3cb2ec8d2afc45
Mhayk Whandson <hi@mhayk.com>
2019-11-28 09:29:17 +0000

    SignOut action has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/TeamSwitcher/index.js
+++ src/components/TeamSwitcher/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import TeamsActions from '~/store/ducks/teams';
+import AuthActions from '~/store/ducks/auth';
 
 import Button from '~/styles/components/Button';
 import Modal from '~/components/Modal';
@@ -19,6 +20,7 @@ class TeamSwitcher extends Component {
     openTeamModal: PropTypes.func.isRequired,
     closeTeamModal: PropTypes.func.isRequired,
     createTeamRequest: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired,
     teams: PropTypes.shape({
       data: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number,
@@ -57,7 +59,9 @@ class TeamSwitcher extends Component {
   }
 
   render() {
-    const { teams, openTeamModal, closeTeamModal } = this.props;
+    const {
+      teams, openTeamModal, closeTeamModal, signOut,
+    } = this.props;
     const { newTeam } = this.state;
 
     return (
@@ -88,7 +92,7 @@ class TeamSwitcher extends Component {
           )}
         </TeamList>
 
-        <Logout onClick={() => {}}>SAIR</Logout>
+        <Logout onClick={signOut}>SAIR</Logout>
       </Container>
     );
   }
@@ -98,6 +102,6 @@ const mapStateToProps = (state) => ({
   teams: state.teams,
 });
 
-const mapDispatchToProps = (dispatch) => bindActionCreators(TeamsActions, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({ ...TeamsActions, ...AuthActions }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TeamSwitcher);
